bcac3db688434c1fa50dbfa2729c5beb55187f50
enluring <enluring@gmail.com>
2014-10-31 16:14:16 +0100

    Nye filer

---
+++ Get-FTP.ps1
@@ -0,0 +1,12 @@
+﻿#FTP
+
+$url = 'ftp.get.com'
+$server = 'Name'
+$username = 'Username'
+$password = 'Password'
+
+
+$cred = New-Object System.Management.Automation.PSCredential -ArgumentList @($username,(ConvertTo-SecureString -String $password -AsPlainText -Force))
+set-ftpconnection -server $server -Credentials $cred -UseBinary -UsePassive
+
+$Files = get-ftpchilditem -path $URL
